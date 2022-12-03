61d9ab89e909209e7600a63784b6377ed1ec1cf5
Jefferson Marioto <90314621+JeffersonMarioto@users.noreply.github.com>
2022-12-03 14:45:43 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,112 @@
+const addForm = document.getElementById("add-user-form");
+const updateForm = document.getElementById("edit-user-form");
+const showAlert = document.getElementById("showAlert");
+const addModal = new bootstrap.Modal(document.getElementById("addNewUserModal"));
+const editModal = new bootstrap.Modal(document.getElementById("editUserModal"));
+const tbody = document.querySelector("tbody");
+
+// Add New User Ajax Request
+addForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(addForm);
+  formData.append("add", 1);
+
+  if (addForm.checkValidity() === false) {
+    e.preventDefault();
+    e.stopPropagation();
+    addForm.classList.add("was-validated");
+    return false;
+  } else {
+    document.getElementById("add-user-btn").value = "Aguarde um momento...";
+
+    const data = await fetch("php/action.php", {
+      method: "POST",
+      body: formData,
+    });
+    const response = await data.text();
+    showAlert.innerHTML = response;
+    document.getElementById("add-user-btn").value = "Add User";
+    addForm.reset();
+    addForm.classList.remove("was-validated");
+    addModal.hide();
+    fetchAllUsers();
+  }
+});
+
+// Fetch All Users Ajax Request
+const fetchAllUsers = async () => {
+  const data = await fetch("php/action.php?read=1", {
+    method: "GET",
+  });
+  const response = await data.text();
+  tbody.innerHTML = response;
+};
+fetchAllUsers();
+
+// Edit User Ajax Request
+tbody.addEventListener("click", (e) => {
+  if (e.target && e.target.matches("a.editLink")) {
+    e.preventDefault();
+    let usuario_id = e.target.getAttribute("usuario_id");
+    editUser(usuario_id);
+  }
+});
+
+const editUser = async (usuario_id) => {
+  const data = await fetch(`php/action.php?edit=1&usuario_id=${usuario_id}`, {
+    method: "GET",
+  });
+  const response = await data.json();
+  document.getElementById("usuario_id").value = response.usuario_id;
+  document.getElementById("usuario").value = response.usuario;
+  document.getElementById("senha").value = response.senha;
+};
+
+// Update User Ajax Request
+updateForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(updateForm);
+  formData.append("update", 1);
+
+  if (updateForm.checkValidity() === false) {
+    e.preventDefault();
+    e.stopPropagation();
+    updateForm.classList.add("was-validated");
+    return false;
+  } else {
+    document.getElementById("edit-user-btn").value = "Por Favor Aguarde...";
+
+    const data = await fetch("php/action.php", {
+      method: "POST",
+      body: formData,
+    });
+    const response = await data.text();
+
+    showAlert.innerHTML = response;
+    document.getElementById("edit-user-btn").value = "Add User";
+    updateForm.reset();
+    updateForm.classList.remove("was-validated");
+    editModal.hide();
+    fetchAllUsers();
+  }
+});
+
+// Delete User Ajax Request
+tbody.addEventListener("click", (e) => {
+  if (e.target && e.target.matches("a.deleteLink")) {
+    e.preventDefault();
+    let id = e.target.getAttribute("usuario_id");
+    deleteUser(id);
+  }
+});
+
+const deleteUser = async (id) => {
+  const data = await fetch(`php/action.php?delete=1&id=${id}`, {
+    method: "GET",
+  });
+  const response = await data.text();
+  showAlert.innerHTML = response;
+  fetchAllUsers();
+};
