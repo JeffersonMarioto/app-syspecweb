b083f51cbd4aa0c4c0cd5cac457e7c3ef5cd4cf9
Jefferson Marioto <90314621+JeffersonMarioto@users.noreply.github.com>
2022-12-03 17:38:56 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'login');
+define('HOST', 'us-cdbr-east-06.cleardb.net');
+define('USUARIO', 'bb5a7d8e29efca');
+define('SENHA', '9da1d24d');
+define('DB', 'heroku_302e836d67b7647');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 ?>
