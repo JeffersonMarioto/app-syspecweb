5545f9442cb0c90d8b5e8668b01a541fe6228d4b
Jefferson Marioto <90314621+JeffersonMarioto@users.noreply.github.com>
2022-12-30 17:38:06 -0300

    Add files
    news e updates

---
+++ scripts/consulta_desmamas.js
@@ -0,0 +1,12 @@
+const showAlert = document.getElementById("showAlert");
+const tbody = document.querySelector("tbody");
+
+// Fetch All Ins.s Ajax Request
+const fetchAllAnimals = async () => {
+  const data = await fetch("php/action_consulta_desmamas.php?read=1", {
+    method: "GET",
+  });
+  const response = await data.text();
+  tbody.innerHTML = response;
+};
+fetchAllAnimals();
